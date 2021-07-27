c4280c1be1f58d52447e75ee2ce86cc576277927
Pranay kumar <67965429+Pranay1234567890@users.noreply.github.com>
2021-07-27 08:54:07 +0530

    updated code
    due to code error i change this code to new one

--- cloudStorage-main/cloudStorage.py
+++ cloudStorage-main/cloudStorage.py
@@ -5,21 +5,22 @@ class TransferData:
         self.access_token = access_token
 
     def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
         dbx = dropbox.Dropbox(self.access_token)
 
-
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
 
 def main():
-    access_token = 'wdC4U8sJJYYAAAAAAAAAAfNGlqZu9i48opKTwhYOiyFKz-_PBPCJux6cBTfKZI-T'
+    access_token = 'v1uR7NDleB8AAAAAAAAAAWdku-QMzfHgIH3OXYRPKX6wjRSPFiY1a4DTexPkn1ts'
     transferData = TransferData(access_token)
 
-    file_from = input("Enter the file path to transfer : -")
-    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+    file_from =  input("ENTER YOUR FILE NAME : ")
+    file_to = input("ENTER YOUR DESTINATION : ")  # The full path to upload the file to, including the file name
 
-    # API v2
+
     transferData.upload_file(file_from, file_to)
-    print("file has been moved !!!")
+    print("FILE UPLOADED")
 
 main()
